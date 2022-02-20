3dd6eb873333bcbae8171cb0fe1a07a2cef908bb
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-02-20 20:12:54 +0300

    added starting server functionality

--- server/index.js
+++ server/index.js
@@ -2,11 +2,26 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
-app.use(cors())
-
+app.use(cors());
 const server = http.createServer(app);
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
+
 server.listen(3001, () => {
-    console.log('SERVER RUNNING')
-})
+  console.log("SERVER RUNNING");
+});
